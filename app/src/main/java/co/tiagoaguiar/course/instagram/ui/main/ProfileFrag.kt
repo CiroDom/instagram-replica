fd70b6c47d3c48aa6f68d28c8c0f4d38ae511a10
Ciro de Oliveira <c.dom.oliveira@gmail.com>
2023-11-12 17:46:01 -0300

    início de recyview

---
+++ app/src/main/java/co/tiagoaguiar/course/instagram/ui/main/ProfileFrag.kt
@@ -0,0 +1,27 @@
+package co.tiagoaguiar.course.instagram.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import co.tiagoaguiar.course.instagram.databinding.FragProfileBinding
+
+class ProfileFrag : Fragment() {
+
+    private val binding by lazy {
+        FragProfileBinding.inflate(layoutInflater)
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
