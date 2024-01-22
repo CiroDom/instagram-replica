45022574210721704ed3f1dd5c8a0f22075ff287
Ciro de Oliveira <c.dom.oliveira@gmail.com>
2024-01-22 18:16:00 -0300

    ui quase pronta

---
+++ app/src/main/java/co/tiagoaguiar/course/instagram/ui/camera/CameraFrag.kt
@@ -0,0 +1,29 @@
+package co.tiagoaguiar.course.instagram.ui.camera
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import co.tiagoaguiar.course.instagram.R
+import co.tiagoaguiar.course.instagram.databinding.FragCameraBinding
+import co.tiagoaguiar.course.instagram.databinding.FragHomeBinding
+
+class CameraFrag : Fragment() {
+
+    private val binding by lazy {
+        FragCameraBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return binding.root
+    }
+
+}
---
+++ app/src/main/java/co/tiagoaguiar/course/instagram/ui/search/SearchFrag.kt
@@ -0,0 +1,49 @@
+package co.tiagoaguiar.course.instagram.ui.search
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import co.tiagoaguiar.course.instagram.R
+import co.tiagoaguiar.course.instagram.databinding.FragSearchBinding
+
+class SearchFrag : Fragment() {
+
+    private val binding by lazy {
+        FragSearchBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val recyView = binding.homefragRecyview
+        with(recyView) {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = ItemSearchAdapter()
+        }
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.profile_dot_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+}
